7808c3279cc836f2937757f4b2424a7e04ca1ed8
Masaya <masaya@matsugasaki.dev>
2025-06-02 22:29:16 +0900

    update css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import 'bulma/css/bulma.min.css';
+
+/* global.css */
+/* v0.0.2 - updated Zen Maru Gothic font */
 import '../styles/globals.css'; // 必要に応じて
 import type { AppProps } from 'next/app';
 
